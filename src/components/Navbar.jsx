f1062cede02afe8897618fbfbffcb5954d629905
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-10-30 00:36:44 +0530

    Attached the text with the image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 import logo from '../assets/react-router-logo.svg';
 import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img className='logo' src={logo} alt="" width="60px"/>
+      <div className='logo-section'>
+        <img className='logo' src={logo} alt="React Router Logo" width="60px" />
         <h1 className='router-text'>router</h1>
-        <ul>
-            <NavLink to="/"><li>Home</li></NavLink>
-            <NavLink to="/about"><li>About</li></NavLink>
-            <NavLink to="/products"><li>Products</li></NavLink>
-            <NavLink to="/contact"><li>Contact</li></NavLink>
-        </ul>
-        <button>Get started</button>
-    </div>
-  )
-}
+      </div>
 
-export default Navbar
+      <ul>
+        <NavLink to="/"><li>Home</li></NavLink>
+        <NavLink to="/about"><li>About</li></NavLink>
+        <NavLink to="/products"><li>Products</li></NavLink>
+        <NavLink to="/contact"><li>Contact</li></NavLink>
+      </ul>
+
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default Navbar;
