400c4a6c8a02ee36f3b1cfa3dff212bb40508a54
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-10-30 00:58:37 +0530

    active class in NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import logo from '../assets/react-router-logo.svg';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import logo from "../assets/react-router-logo.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='logo-section'>
-        <img className='logo' src={logo} alt="React Router Logo" width="60px" />
-        <h1 className='router-text'>router</h1>
+    <div className="navbar">
+      <div className="logo-section">
+        <img className="logo" src={logo} alt="React Router Logo" width="60px" />
+        <h1 className="router-text">router</h1>
       </div>
 
       <ul>
-        <NavLink to="/"><li>Home</li></NavLink>
-        <NavLink to="/about"><li>About</li></NavLink>
-        <NavLink to="/products"><li>Products</li></NavLink>
-        <NavLink to="/contact"><li>Contact</li></NavLink>
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
+        >
+          <li>Home</li>
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
+        >
+          <li>About</li>
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
+        >
+          <li>Products</li>
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
+        >
+          <li>Contact</li>
+        </NavLink>
       </ul>
 
       <button>Get Started</button>
