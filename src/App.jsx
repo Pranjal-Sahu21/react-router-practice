fde42e9edc63814edca21de1a2d6604351cd6a0d
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-10-30 23:51:01 +0530

    Added a custom not found page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import RootLayout from "./layout/RootLayout";
 import ContactLayout from "./layout/ContactLayout";
 import ContactInfo from "./components/ContactInfo";
 import ContactForm from "./components/ContactForm";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="info" element={<ContactInfo/>}/>
           <Route path="form" element={<ContactForm/>}/>
         </Route>
+        <Route path="*" element={<NotFound/>}/>
       </Route>
     )
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1>Error-404</h1>
+        <h2>Not Found</h2>
+        <h4>Sorry, this page does not exist</h4>
+    </div>
+  )
+}
+
+export default NotFound
