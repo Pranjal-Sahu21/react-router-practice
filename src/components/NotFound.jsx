983ec3923e74ab64fe46c1bacd30d9749e22453a
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-10-31 00:00:55 +0530

    Added a go home button in the notfound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate();  
   return (
     <div>
         <h1>Error-404</h1>
         <h2>Not Found</h2>
         <h4>Sorry, this page does not exist</h4>
+        <button onClick={() => navigate('/')} className='return-button'>Go home</button>
     </div>
   )
 }
