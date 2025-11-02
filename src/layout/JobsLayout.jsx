48fb254e566c849682e3a9a2599823cdffa98e39
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-11-02 17:00:34 +0530

    Some more styling changes

--- src/layout/JobsLayout.jsx
+++ src/layout/JobsLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 const JobsLayout = () => {
   return (
     <div>
-        <h1 style={{fontSize:"40px"}}>Job openings</h1>
+        <h1>Job openings</h1>
         <Outlet/>
     </div>
   )
